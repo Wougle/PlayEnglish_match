1d2c64debdd6bbafb9db63c16eafa9a56c0d5f9d
吴戈 <wougle@163.com>
2017-04-16 09:53:11 +0800

    playEnglish比赛项目转移

---
+++ PlayEnglish_match/PlayEnglish_iOS/Class/Module/Community/ViewControllers/PESubmitViewController.h
@@ -0,0 +1,17 @@
+//
+//  PESubmitViewController.h
+//  PlayEnglish_iOS
+//
+//  Created by 吴戈 Wougle on 2016/12/5.
+//  Copyright © 2016年 DMT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GCPlaceholderTextView.h"
+@interface PESubmitViewController : UIViewController{
+    GCPlaceholderTextView *titleTextView;
+}
+
+@property (nonatomic, assign) int commIdLocal;
+
+@end
